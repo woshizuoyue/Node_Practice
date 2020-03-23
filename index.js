c10d7d77e02179353cacb2e8dedf4ae8551828ad
Yue Zuo <woshizuoyue9201@yahoo.com>
2020-03-23 00:14:41 -0400

    product

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ const replaceTemplate = (temp, product)=>{
     output = output.replace(/{%FROM%}/g, product.from);
     output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
     output = output.replace(/{%QUANTITY%}/g, product.quantity);
-    output = output.replace(/{%DISCRIPTION%}/g, product.discription);
+    output = output.replace(/{%DESCRIPTION%}/g, product.description);
     output = output.replace(/{%ID%}/g, product.id);
 
     if(!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
@@ -57,10 +57,11 @@ const dataObj = JSON.parse(data);
 
 const server = http.createServer((req, res) =>{
 
-    const pathName = req.url;
+    
+    const {query, pathname} = url.parse(req.url, true);
 
     // overview
-    if(pathName === '/' || pathName === '/overview'){
+    if(pathname === '/' || pathname === '/overview'){
 
         res.writeHead(200, {'Content-type': 'text/html'});
 
@@ -72,13 +73,16 @@ const server = http.createServer((req, res) =>{
     }
 
     // product
-    else if(pathName === '/product'){
+    else if(pathname === '/product'){
 
-        res.end('This is product');
+        res.writeHead(200, {'Content-type':'text/html'});
+        const product = dataObj[query.id];
+        const output = replaceTemplate(tempProduct, product);
+        res.end(output);
     }
 
     // api
-    else if(pathName === '/api'){
+    else if(pathname === '/api'){
 
         res.writeHead(200, {'Content-type': 'application/json'});
         res.end(data);
